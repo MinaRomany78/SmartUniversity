1c7c7bd92111ed1f1cd735779d87a254695238f0
Mina Romany <minaromany155@gmail.com>
2025-08-09 12:37:07 +0300

    Revert "startPage"

--- SmartUniversity/Program.cs
+++ SmartUniversity/Program.cs
@@ -22,11 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-
-    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}")
-
     pattern: "{controller=Home}/{action=Index}/{id?}")
-
     .WithStaticAssets();
 
 
