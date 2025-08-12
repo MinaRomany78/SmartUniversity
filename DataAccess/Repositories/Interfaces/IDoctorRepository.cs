501b2591c31ba63806f1a079015f3c149995cf71
Mina Romany <minaromany155@gmail.com>
2025-08-12 09:45:44 +0300

    Delete DataAccess/Repositories/Interfaces directory

--- DataAccess/Repositories/Interfaces/IDoctorRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Entities.Models;
-
-namespace SmartUniversity.Repositories.Interfaces
-{
-    public interface IDoctorRepository : IGenericRepository<Doctor>
-    {
-        Task<Doctor?> GetDoctorWithCoursesAsync(int id);
-    }
-}
