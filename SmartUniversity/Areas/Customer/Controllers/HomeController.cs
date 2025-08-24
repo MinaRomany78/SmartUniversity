d0f49ad01b017ba481b1d4c6722b59479fbc1603
MinaRomany78 <minaromany155@gmail.com>
2025-08-24 16:51:34 +0300

    Design modification

--- SmartUniversity/Areas/Customer/Controllers/HomeController.cs
+++ SmartUniversity/Areas/Customer/Controllers/HomeController.cs
@@ -8,31 +8,6 @@ namespace SmartUniversity.Areas.Customer.Controllers
     {
         public IActionResult Index()
         {
-            if (User.Identities is null)
-            {
-                return View();
-            }
-
-            if (User.IsInRole($"{SD.UniversityStudent}"))
-            {
-                return RedirectToAction("index", "UniversityStudent");
-            }
-            else if (User.IsInRole($"{SD.Doctor}"))
-            {
-                return RedirectToAction("index", "DoctorAndAssistant");
-            }
-
-            else if (User.IsInRole($"{SD.Assistant}"))
-            {
-                return RedirectToAction("index", "DoctorAndAssistant");
-            }
-            else if (User.IsInRole($"{SD.ExternalStudent}"))
-            {
-                return RedirectToAction("index", "ExternalStudent");
-            }
-
-
-            // لو مفيش Role مناسب
             return View();
         }
 
