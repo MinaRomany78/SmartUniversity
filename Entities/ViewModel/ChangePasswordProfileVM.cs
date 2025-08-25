224e4520c63b913b1e4eba90233226d6cabd5867
Ahmedkh81 <ahmedkmuhamedd@gmail.com>
2025-08-25 20:52:47 +0300

    Adding Account area to Manage user profile, Adding a table for admins & Adding toggleBlock for all users

---
+++ Entities/ViewModel/ChangePasswordProfileVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ViewModel
+{
+    public class ChangePasswordProfileVM
+    {
+        [Required]
+        public string CurrentPassword { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; } = null!;
+
+        [Compare("NewPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
